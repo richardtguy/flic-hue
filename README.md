8a03ed478775448e3df5fef4c422fbbaa799b263
Richard Guy <rich.guy@cantab.net>
2016-05-20 08:23:26 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# flic-hue
-Use flic Bluetooth buttons to control Philips hue lights via a Raspberry Pi 3.
+*Overview
+- This application enables flic bluetooth LE buttons to be used to control Philips hue smart lights via a Raspberry Pi 3 running Rasbian.
+
+- Verified buttons communicates with the Bluetooth controller on the Raspberry Pi via a server application which serves click events to a client application.  The client queries the hue bridge API to switch lights on or off depending on the click type (single click or hold).  Buttons are associated with groups of lights as defined in a JSON formatted file.
+
+*Instructions
+
+- In one terminal, start flic server (as root)
+`cd server`
+`sudo ./flicd -f flic.sqlite3`
+
+- To connect new buttons, in another terminal, `cd simpleclient`, compile it with `make` and run with `./simpleclient localhost`. You will be shown the available commands. Type `startScan` and press enter to start scanning for buttons. Then press your flic button (and make sure it is disconnected to any other devices such as a smartphone) and you should see it appear. Type `stopScan` and press enter to stop scanning (it's ok if output text are interleaved with what you type). Hold your Flic button for 7 seconds to make it public, and make sure that it glows red. Then enter the command connect `<BDADDR> <id>` where `<BDADDR>` is the address that appeared during scan. For `<id>`, put any integer that will be used later to refer to this connection. The button should now connect and you will see click events appear. Type `disconnect <id>` to later disconnect.
+
+- To run the light switch client, `cd client` and run with `./my_client.py`
+
+- Press and hold any button to switch off all lights
+
+- To turn on a group of lights when a button is single-clicked, add the button and a list of light IDs to the 'groups' file.  Light IDs can be obtained by querying the /lights resource on the hue bridge.
+```
+{
+    "80:e4:da:71:36:f6": {
+        "group": [
+            4,
+            5,
+            6
+        ]
+    }
+}
+```
+
+*References
+flic button server & client libraries: `https://github.com/50ButtonsEach/fliclib-linux-hci`
+Philips hue API: `http://www.developers.meethue.com/philips-hue-api`
+Python library for Philips hue: `https://github.com/quentinsf/qhue`
+
---
+++ client/my_client.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+# Flic button client application to control Philips hue lights.
+#
+# This program attempts to connect to all previously verified Flic buttons by this server.
+# Once connected, it uses the Qhue libtrary to query the Hue bridge API when (any) button is clicked.
+# It also monitors when new buttons are verified and connects to them as well. For example, run this program and at the same time the scan_wizard.py program.
+
+# To do:
+#  - choose scenes for single click, double click & hold
+#  - implement APIn calls
+
+import json
+
+# import flic client library
+import fliclib
+
+# import QHue libraries for Philips Hue API
+from os import path
+from qhue import Bridge, QhueException, create_new_username
+
+# the IP address and username of Hue bridge
+BRIDGE_IP = "192.168.1.1"
+# the path for the username credentials file
+CRED_FILE_PATH = "hue_username"
+
+# check for a credential file
+if not path.exists(CRED_FILE_PATH):
+
+	while True:
+		try:
+			username = create_new_username(BRIDGE_IP)
+			break
+		except QhueException as err:
+			print ("Error occurred while creating a new username: {}").format(err)
+
+	# store the username in a credential file
+	with open(CRED_FILE_PATH, "w") as cred_file:
+		cred_file.write(username)
+
+else:
+	print ('Getting username from file...')
+	with open(CRED_FILE_PATH, "r") as cred_file:
+		username = cred_file.read()
+		print ('username: ' + username)
+
+# create the bridge resource, passing the captured username
+bridge = Bridge(BRIDGE_IP, username)
+# create a lights resource
+lights = bridge.lights
+print (lights.url)
+
+# load button groups from file
+with open('groups') as f:
+	json_data = f.read()
+groups = json.loads(json_data)
+
+# create flic client
+client = fliclib.FlicClient("localhost")
+
+def click_handler(channel, click_type, was_queued, time_diff):
+	print(channel.bd_addr + " " + str(click_type))
+	if str(click_type) == 'ClickType.ButtonSingleClick':
+		print ("Switching on associated lights... " + str(groups[channel.bd_addr]['group']))
+		for l in groups[channel.bd_addr]['group']:
+			s = bridge.lights[l].state
+			s(on=True)
+	elif str(click_type) == 'ClickType.ButtonHold':
+		# turn off all lights
+		print ("Turning off all lights...")
+		for l in lights():
+			s = bridge.lights[l].state
+			s(on=False)
+	return
+
+def got_button(bd_addr):
+	cc = fliclib.ButtonConnectionChannel(bd_addr)
+	# Assign function to call when a button is clicked
+	cc.on_button_single_or_double_click_or_hold = click_handler
+	cc.on_connection_status_changed = \
+		lambda channel, connection_status, disconnect_reason: \
+			print(channel.bd_addr + " " + str(connection_status) + (" " + str(disconnect_reason) if connection_status == fliclib.ConnectionStatus.Disconnected else ""))
+	client.add_connection_channel(cc)
+
+def got_info(items):
+	print(items)
+	for bd_addr in items["bd_addr_of_verified_buttons"]:
+		got_button(bd_addr)
+
+client.get_info(got_info)
+
+client.on_new_verified_button = got_button
+
+client.handle_events()
